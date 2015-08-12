3d5c302b26125ed434339859107b49f61e65c85c
Richard Craggs <rcraggs@gmail.com>
2015-08-12 10:52:48 +0100

    Fixed test

--- src/main/java/rc/app/HelloWorld.java
+++ src/main/java/rc/app/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");    
     }
+
+    public int add(int a, int b)  {
+        return a+b;
+    }
 }
---
+++ src/test/java/rc/app/AddTest.java
@@ -0,0 +1,17 @@
+package rc.app;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AddTest {
+
+  @Test
+  public void testAdd() {
+
+    // MyClass is tested
+    HelloWorld tester = new HelloWorld();
+
+    // assert statements
+    assertEquals(10, tester.add(10, 0));
+  }
+
+}
